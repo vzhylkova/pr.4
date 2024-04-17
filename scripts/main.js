66131258a77441e4c83480319fbe362eabfd4052
vzhylkova <vikazhylkova@gmail.com>
2024-04-17 16:20:27 +0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/planets1.png') {
+        myImage.setAttribute('src', 'images/planets2.png');
+    } else {
+        myImage.setAttribute('src', 'images/planets1.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('Please enter your name');
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.innerHTML = 'Welcome,' + myName;
+    }
+}
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Welcome,' + storedName;
+ }
+
+myButton.onclick = function () {
+    setUserName();
+ }
